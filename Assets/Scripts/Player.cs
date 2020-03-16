b8146790347ac7b9bdb7c65136c865808908698b
EgorNeugodov <eneugodov@inbox.ru>
2020-03-16 20:52:17 +0700

    first commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,141 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour {
+    public float WalkSpeed;
+    public float JumpForce;
+    public AnimationClip _walk, _jump;
+    public Animation _Legs;
+    public Transform _Blade, _GroundCast;
+    public Camera cam;
+    public bool mirror;
+
+    public Joystick joystick;
+
+
+    bool facingRight = true;
+
+    private bool _canJump, _canWalk;
+    private bool _isWalk, _isJump;
+    private float rot, _startScale;
+    private Rigidbody2D rig;
+    private Vector2 _inputAxis;
+    private RaycastHit2D _hit;
+
+    
+
+	void Start ()
+    {
+        rig = gameObject.GetComponent<Rigidbody2D>();
+        _startScale = transform.localScale.x;
+	}
+
+    void Update()
+    {
+        if (_hit = Physics2D.Linecast(new Vector2(_GroundCast.position.x, _GroundCast.position.y + 0.2f), _GroundCast.position))
+        {
+            if (!_hit.transform.CompareTag("Player"))
+            {
+                _canJump = true;
+                _canWalk = true;
+            }
+        }
+        else _canJump = false;
+
+        _inputAxis = new Vector2(joystick.Horizontal, joystick.Vertical);
+        if (_inputAxis.y > 0.3f && _canJump)
+        {
+            _canWalk = false;
+            _isJump = true;
+        }
+
+
+        if (_inputAxis.x < 0 && facingRight)
+        {
+            flip();
+        }
+
+        else if (_inputAxis.x > 0 && !facingRight)
+        {
+            flip();
+        }
+
+
+        if (transform.position.y < -230)
+        {
+            SceneManager.LoadScene("StartVideo");
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+
+
+        /*Vector3 dir = cam.ScreenToWorldPoint(Input.mousePosition) - _Blade.transform.position;
+        dir.Normalize();
+
+        if (cam.ScreenToWorldPoint(Input.mousePosition).x > transform.position.x + 0.2f)
+            mirror = false;
+        if (cam.ScreenToWorldPoint(Input.mousePosition).x < transform.position.x - 0.2f)
+            mirror = true;
+
+        if (!mirror)
+        {
+            rot = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            
+        }
+        if (mirror)
+        {
+            rot = Mathf.Atan2(-dir.y, -dir.x) * Mathf.Rad2Deg;
+            
+        }*/
+
+
+
+        if (_inputAxis.x != 0)
+        {
+            rig.velocity = new Vector2(_inputAxis.x * WalkSpeed * Time.deltaTime, rig.velocity.y);
+
+            if (_canWalk)
+            {
+                _Legs.clip = _walk;
+                _Legs.Play();
+            }
+        }
+
+        else
+        {
+            rig.velocity = new Vector2(0, rig.velocity.y);
+        }
+
+        if (_isJump)
+        {
+            rig.AddForce(new Vector2(0, JumpForce));
+            _Legs.clip = _jump;
+            _Legs.Play();
+            _canJump = false;
+            _isJump = false;
+        }
+    }
+
+    public bool IsMirror()
+    {
+        return mirror;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, _GroundCast.position);
+    }
+
+    void flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0f, 180f, 0f);
+    }
+}
